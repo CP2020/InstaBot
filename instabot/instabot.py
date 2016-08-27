00cb97680f97cb289b6bef5982f748a4d54f94c4
Pyotr Ermishkin <quasiyoke@gmail.com>
2016-08-27 23:19:39 +0300

    Bumped version number to 0.3.0.

--- instabot/instabot.py
+++ instabot/instabot.py
@@ -29,7 +29,7 @@ Arguments:
   CONFIGURATION  Path to configuration.yml file.
 '''
 LOGGER = logging.getLogger('instabot')
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 
 
 def install(configuration, db):
