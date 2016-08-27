1a710782c851a0a03b272736cef0a6bb8f944dc4
Pyotr Ermishkin <quasiyoke@gmail.com>
2016-08-27 16:23:09 +0300

    "unfollow" command was added.

--- instabot/instabot.py
+++ instabot/instabot.py
@@ -22,6 +22,7 @@ DOC = '''InstaBot
 Usage:
   instabot CONFIGURATION
   instabot install CONFIGURATION
+  instabot unfollow CONFIGURATION
   instabot -h | --help | --version
 
 Arguments:
@@ -32,6 +33,7 @@ __version__ = '0.2.2'
 
 
 def install(configuration, db):
+    LOGGER.info('Installing InstaBot')
     db.create_tables([User])
     client = instagram.Client(configuration)
     now = datetime.datetime.utcnow()
@@ -44,19 +46,8 @@ def install(configuration, db):
         # To prevent attempts to follow user by himself.
         was_followed_at=was_followed_at,
         )
-    loop = asyncio.get_event_loop()
-    followed_users_json = loop.run_until_complete(client.get_followed(user))
-    for followed_json in followed_users_json:
-        User.create(
-            following_depth=0,
-            instagram_id=followed_json['id'],
-            is_followed=True,
-            username=followed_json['username'],
-            was_followed_at=was_followed_at,
-            )
-    LOGGER.info(
-        '{0} followed users were saved in DB'.format(len(followed_users_json)),
-        )
+
+    unfollow(configuration)
 
 
 def main():
@@ -70,14 +61,15 @@ def main():
     db = get_db(configuration)
 
     if arguments['install']:
-        LOGGER.info('Installing InstaBot')
         install(configuration, db)
+    elif arguments['unfollow']:
+        unfollow(configuration)
     else:
-        LOGGER.info('Executing InstaBot')
         run(configuration)
 
 
 def run(configuration):
+    LOGGER.info('Executing InstaBot')
     loop = asyncio.get_event_loop()
 
     stats_service = StatsService()
@@ -106,3 +98,29 @@ def run(configuration):
         loop.create_task(like_service.run())
 
     loop.run_forever()
+
+
+def unfollow(configuration):
+    LOGGER.info('Scheduling unfollowing of everyone')
+    client = instagram.Client(configuration)
+    loop = asyncio.get_event_loop()
+    followed_users_json = loop.run_until_complete(client.get_followed(
+        User.get(instagram_id=client.id),
+        ))
+    now = datetime.datetime.utcnow()
+    was_followed_at = now - \
+        datetime.timedelta(hours=configuration.following_hours)
+    for followed_json in followed_users_json:
+        try:
+            user = User.get(instagram_id=followed_json['id'])
+        except User.DoesNotExist:
+            user = User(instagram_id=followed_json['id'])
+        user.username = followed_json['username']
+        user.following_depth = 0
+        user.is_followed = True
+        if not user.was_followed_at or was_followed_at < user.was_followed_at:
+            user.was_followed_at = was_followed_at
+        user.save()
+    LOGGER.info(
+        '{0} followed users were saved in DB'.format(len(followed_users_json)),
+        )
