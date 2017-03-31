77b7a66f3c3f68012028222c7b582050053ff350
Pyotr Ermishkin <quasiyoke@gmail.com>
2017-03-31 23:56:33 +0300

    Bumping version number to 0.3.5. Error response about temporary blocking is JSON encoded now.

--- instabot/instabot.py
+++ instabot/instabot.py
@@ -28,7 +28,7 @@ Arguments:
   CONFIGURATION  Path to configuration.yml file.
 '''
 LOGGER = logging.getLogger('instabot')
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 
 def install(configuration, db):
